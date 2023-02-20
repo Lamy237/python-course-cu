7546b276a19d1d0e69f4565f444d5c9a65984f89
Lamy237 <89041260+Lamy237@users.noreply.github.com>
2023-02-20 23:36:25 +0530

    upload code file

---
+++ 04_inputs/main.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+drive_mechanism = input('What is the most efficient drive mechanism? ')
+
+print(f'I am {name} and I think {drive_mechanism} is the most efficient reservoir drive mechanism.')
