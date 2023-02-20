8038a7cbe0b9bbd6d2e7aa5eb2f6735764b12016
Lamy237 <89041260+Lamy237@users.noreply.github.com>
2023-02-20 23:26:10 +0530

    upload code file

---
+++ 03_outputs/main.py
@@ -0,0 +1,9 @@
+area = 2000
+thickness = 150
+porosity = 0.15
+water_saturation = 0.3
+
+print(f'Area of the reservoir: {area} acres')
+print(f'Thickness: {thickness} ft')
+print(f'Porosity: {porosity*100:.0f}%')
+print(f'Water saturation: {water_saturation*100:.0f}%')
